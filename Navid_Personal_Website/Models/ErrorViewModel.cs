6d9bfdc7aaeab4e9ab534cd0531629c5e9993183
Navid Ghavami <navid_ghavami@outlook.com>
2025-12-10 11:25:24 +0330

    Add project files.

---
+++ Navid_Personal_Website/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Navid_Personal_Website.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
