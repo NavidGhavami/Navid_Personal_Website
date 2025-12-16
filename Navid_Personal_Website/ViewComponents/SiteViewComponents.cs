4475028cda6341490c317a4041a0a0e0c3b6fca6
Navid Ghavami <navid_ghavami@outlook.com>
2025-12-16 18:00:41 +0330

    The Main Template has been Partinioned and View Component Added to Project.

---
+++ Navid_Personal_Website/ViewComponents/SiteViewComponents.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Navid_Personal_Website.ViewComponents
+{
+    #region My Information
+
+    public class MyInformationViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("MyInformation");
+        }
+    }
+
+    #endregion
+
+    #region My Service
+
+    public class MyServiceViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("MyService");
+        }
+    }
+
+    #endregion
+
+    #region My Resume
+
+    public class MyResumeViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("MyResume");
+        }
+    }
+
+    #endregion
+
+    #region My Skill
+
+    public class MySkillViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("MySkill");
+        }
+    }
+
+    #endregion
+
+    #region My Project
+
+    public class MyProjectViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("MyProject");
+        }
+    }
+
+    #endregion
+
+    #region My Blog
+
+    public class MyBlogViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("MyBlog");
+        }
+    }
+
+
+    #endregion
+
+    #region My Contact Me
+
+    public class ContactMeViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("ContactMe");
+        }
+    }
+
+
+    #endregion
+}
